0f105c9783f4338f92e482254d26030b4ea9d17e
Olatunji Yusuf <olatunjiyso@gmail.com>
2024-05-26 20:34:20 +0100

    add payments components

---
+++ app/dashboard/payments/page.tsx
@@ -0,0 +1,36 @@
+import Pagination from '@/app/ui/payments/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/payments/table';
+import { josefin } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchPaymentsPages } from '@/app/lib/data';
+
+ 
+export default async function Page({ searchParams}: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    }
+}) {
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchPaymentsPages(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${josefin.className} text-2xl`}>Payments</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search payments by name or email..." />
+      </div>
+       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
+}
