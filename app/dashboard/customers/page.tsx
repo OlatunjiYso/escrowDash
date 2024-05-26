0be7ddfc5ff6c8d6ca9a7b9e82bcfd02f398d801
Olatunji Yusuf <olatunjiyso@gmail.com>
2024-05-26 20:35:38 +0100

    add dashboard components for customers

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import { josefin } from '@/app/ui/fonts';
+
+
+ 
+export default async function Page() {
+  return (
+    <div className=" flex items-center justify-center mt-12">
+  <h3 className='text-3xl'> Coming soon 😊</h3>
+</div>
+  );
+}
