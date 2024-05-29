5554e09cac778200f38a75b950e3e21b0fb61999
Olatunji Yusuf <olatunjiyso@gmail.com>
2024-05-29 23:46:00 +0100

    fix status dropdown styles

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -29,7 +29,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   return (
     <div className="relative flex flex-1 flex-shrink-0 justify-between">
-      <div className=" flex sm:grow md:w-6/12 lg:w-8/12">
+      <div className=" flex grow md:w-6/12 lg:w-8/12">
         <label htmlFor="search" className="sr-only">
           Search
         </label>
@@ -44,7 +44,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
       </div>
       <select
-      className="rounded-md border border-gray-200"
+      className="rounded-md border border-gray-200 grow"
        
         onChange={(e) => handleStatusChange(e.target.value)}
         value={(searchParams.get("status") || 'all').toString()}
