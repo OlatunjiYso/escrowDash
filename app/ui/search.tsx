411c93d1da12c411fb2283440593dcdc918f3096
Olatunji Yusuf <olatunjiyso@gmail.com>
2024-06-05 20:15:08 +0100

    fix dropdown for status toggle

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -20,16 +20,16 @@ export default function Search({ placeholder }: { placeholder: string }) {
     replace(`${pathname}?${params.toString()}`);
   }, 600);
 
-  const handleStatusChange = (newStatus:string)=> {
+  const handleStatusChange = (newStatus: string) => {
     const params = new URLSearchParams(searchParams);
     params.set("page", "1");
     params.set("status", newStatus);
     replace(`${pathname}?${params.toString()}`);
-  }
+  };
 
   return (
-    <div className="relative flex flex-1 flex-shrink-0 justify-between">
-      <div className=" flex grow md:w-6/12 lg:w-8/12">
+    <div className="md:relative sm:block lg:flex flex-1 flex-shrink-0 justify-between">
+      <div className="sm:w-12/12 lg:flex grow lg:w-8/12 mb-6 lg:mb-0">
         <label htmlFor="search" className="sr-only">
           Search
         </label>
@@ -41,20 +41,19 @@ export default function Search({ placeholder }: { placeholder: string }) {
           }}
           defaultValue={searchParams.get("query")?.toString()}
         />
-        <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+        <MagnifyingGlassIcon className="hidden lg:block absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
       </div>
-      <select
-      className="rounded-md border border-gray-200 grow"
-       
-        onChange={(e) => handleStatusChange(e.target.value)}
-        value={(searchParams.get("status") || 'all').toString()}
-      >
-        <option value="all">All Payments</option>
-        <option value="paid"> Successful</option>
-        <option value="pending">Pending</option>
-        <option value="disputed">Disputed</option>
-        <option value="returned">Returned</option>
-      </select>
+        <select
+          className="rounded-md border border-gray-200 grow w-9/12 md:w-6/12 lg:w-3/12 xl:w-2/12"
+          onChange={(e) => handleStatusChange(e.target.value)}
+          value={(searchParams.get("status") || "all").toString()}
+        >
+          <option value="all">All Payments</option>
+          <option value="paid"> Successful</option>
+          <option value="pending">Pending</option>
+          <option value="disputed">Disputed</option>
+          <option value="returned">Returned</option>
+        </select>
     </div>
   );
 }
